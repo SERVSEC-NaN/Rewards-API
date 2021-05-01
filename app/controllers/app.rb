59a081e0153069739a1a1212b03645e519aa88a7
Brandon Duval Benn <me@brandonbenn.xyz>
2021-05-01 19:26:38 +0800

    refactor: minor changes

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -14,12 +14,12 @@ module Rewards
     route do |routing|
       routing.root do
         response.status = 200
-        { message: "Rewards API accessible at /#{@api_root}/" }.to_json
+        { message: "Rewards API accessible at /#{@api_root}/" }
       end
 
       routing.on @api_root do
-        %w[subscribers subscriptions promoters tags].each do |route_name|
-          handle_endpoint routing, route_name
+        %w[Subscriber Subscription Promoter Tag].each do |model_route|
+          handle_endpoint routing, model_route
         end
       end
     end
@@ -29,7 +29,7 @@ module Rewards
     def handle_get_id(id, name, model)
       model[id]
     rescue StandardError
-      r.halt 404, { message: "#{name.capitalize} Not Found" }
+      r.halt 404, { message: "#{name} Not Found" }
     end
 
     def handle_get(model)
@@ -38,10 +38,10 @@ module Rewards
     end
 
     def handle_post(id, name, model)
-      message = "#{name.capitalize} Stored"
-      unless model.create JSON.parse(r.params)
-        message = "Could Not Create #{name.capitalize}"
-        r.halt 400, { message: message }
+      message = "#{name} Stored"
+      unless model.create routing.params
+        message = "Could Not Create #{name}"
+        routing.halt 400, { message: message }
       end
 
       response.status = 201
@@ -50,14 +50,12 @@ module Rewards
     end
 
     def handle_endpoint(routing, model_name)
-      model_class = Object.const_get model_name.capitalize
+      model_class = Object.const_get model_name
 
-      routing.on "#{model_name}s" do
+      routing.on "#{model_name.downcase}s" do
         routing.is Integer do |id|
           routing.get { handle_get_id id, model_name, model_class }
-
           routing.get { handle_get model_class }
-
           routing.post  { handle_post id, model_name, model_class }
         end
       end
