9b2857d8ab4192d126e6d30efd8986d940c51149
Brandon Duval Benn <me@brandonbenn.xyz>
2021-05-03 00:20:32 +0800

    wip: integration tests for models

---
+++ spec/spec_helper.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+ENV['RACK_ENV'] = 'test'
+
+require 'minitest/autorun'
+require 'minitest/rg'
+require 'yaml'
+
+require_relative 'test_load_all'
+
+TABLES = %i[subscribers subscriptions promoters tags].freeze
+
+def wipe_database
+  TABLES.each do |table|
+    app.DB[table].delete
+  end
+end
---
+++ spec/test_load_all.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'rack/test'
+require_relative '../require_app'
+
+require_app
+
+include Rack::Test::Methods # rubocop:disable Style/MixinUsage
+
+def app = Rewards::Api
