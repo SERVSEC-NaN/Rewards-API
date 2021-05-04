98ffb6d9ca299da1d47764c4c3903ffdf0f6e9cf
Brandon Duval Benn <me@brandonbenn.xyz>
2021-05-05 00:26:01 +0800

    wip: api for models

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -2,26 +2,32 @@
 
 require 'roda'
 require 'json'
-require_relative '../models/subscriber'
 
 module Rewards
-  # Web controller for Rewards api
+  # Web controller for Rewards API
   class Api < Roda
-    plugin :all_verbs
-    plugin :halt
-    plugin :json
+    @plugins = %i[all_verbs halt json].freeze
+    @models = %w[subscriber subscription promoter tag].freeze
+    @api_root = 'api/v1'
 
-    MODELS = %w[Subscriber Subscription Promoter Tag].freeze
+    @plugins.each { |p| plugin p }
 
-    api_root = 'api/v1'
-    route do |routing|
-      routing.root do
-        { message: "Rewards API accessible at /#{api_root}/" }
+    route do |r|
+      r.root do
+        { message: "Rewards API accessible at /#{@api_root}/" }
       end
 
-      routing.on "#{api_root}" do
-        MODELS.each do |model_route|
-          handle_endpoint routing, model_route
+      r.on @api_root do
+        @models.each do |model_name|
+          model = Object.const_get model_name.capitalize
+
+          r.on "#{model_name}s" do
+            r.get { handle_get model }
+            r.is Integer do |model_id|
+              r.get  { handle_get model_id, model }
+              r.post { handle_post model_id, model }
+            end
+          end
         end
       end
     end
@@ -29,12 +35,11 @@ module Rewards
     private
 
     def handle_get(model, id = nil)
-      return model[id] || raise("#{model.to_s.downcase} not found") if id
+      return model[id] || raise("#{model.to_s.downcase}s/#{id} not found") if id
 
       response.status = 200
       model.all
-    rescue StandardError => e
-      routing.halt 404, { message: e.message }
+    rescue StandardError => e; routing.halt 404, { message: e.message }
     end
 
     def handle_post(id, model)
@@ -42,20 +47,7 @@ module Rewards
       model.create routing.params || raise("could not create #{name}")
       response.status = 201
       { message: "#{name} stored", id: id }
-    rescue StandardError => e
-      routing.halt 400, { message: e.message }
-    end
-
-    def handle_endpoint(routing, model_name)
-      model = Object.const_get model_name
-
-      routing.on "#{model_name.downcase}s" do
-        routing.is Integer do |id|
-          routing.get   { handle_get model }
-          routing.get   { handle_get id, model }
-          routing.post  { handle_post id, model }
-        end
-      end
+    rescue StandardError => e; routing.halt 400, { message: e.message }
     end
   end
 end
