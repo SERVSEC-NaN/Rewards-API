46667e36a3842f92fe6332bfc3413fa69b8112a4
Brandon Duval Benn <brandon@codegiant.io>
2021-05-15 18:41:44 +0800

    fix: foreign key type to accomodate UUID

--- app/db/migrations/004_create_subscriptions.rb
+++ app/db/migrations/004_create_subscriptions.rb
@@ -6,7 +6,8 @@ Sequel.migration do
   change do
     create_table(:subscriptions) do
       uuid :id, primary_key: true
-      foreign_key :promoter_id, table: :promoters
+      foreign_key :promoter_id, table: :promoters,
+                                type: 'char(36)' # UUID
       String :title, null: false
       String :description, null: false
     end
