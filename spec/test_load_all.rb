ed8f5668196e7eea75775a4ccc5265d008762a59
Brandon Duval Benn <me@brandonbenn.xyz>
2021-05-28 02:35:38 +0800

    test: excluded rack test in production environment

--- spec/test_load_all.rb
+++ spec/test_load_all.rb
@@ -5,6 +5,9 @@ require_relative '../config/require_app'
 
 require_app
 
-include Rack::Test::Methods # rubocop:disable Style/MixinUsage
-
 def app = Rewards::Api
+
+unless app.environment == :production
+  require 'rack/test'
+  include Rack::Test::Methods # rubocop:disable Style/MixinUsage
+end
