c4ea31469c5b852c4fd1af88d8d01455a702ff30
Brandon Duval Benn <me@brandonbenn.xyz>
2021-05-27 18:53:15 +0800

    test: unit tests password model

---
+++ spec/unit/password_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require_relative '../spec_helper'
+
+describe 'Test Password Digestion' do
+  before do
+    wipe_database
+    @password1 = 'ediblesofunusualsizecolorandtexture'
+    @password2 = 'myuncannylittlepremonitionaboutlife'
+  end
+
+  it 'SECURITY: create password digests safely, hiding raw password' do
+    digest = Rewards::Password.digest(@password1)
+    assert_equal false, digest.to_s.match?(@password1)
+  end
+
+  it 'SECURITY: successfully checks correct password from stored digest' do
+    digest_s = Rewards::Password.digest(@password1).to_s
+    digest   = Rewards::Password.from_digest(digest_s)
+    assert_equal true, digest.correct?(@password1)
+  end
+
+  it 'SECURITY: successfully detects incorrect password from stored digest' do
+    digest_s1 = Rewards::Password.digest(@password1).to_s
+    digest1   = Rewards::Password.from_digest(digest_s1)
+    assert_equal false, digest1.correct?(@password2)
+  end
+end
